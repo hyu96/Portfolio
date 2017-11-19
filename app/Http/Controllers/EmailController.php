39026c6fa0389e3d9ff7868586f5d9401c709efe
Huy <huydq2510@gmail.com>
2017-11-19 21:31:50 +0700

    Change email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -16,11 +16,6 @@ class EmailController extends Controller
         $guest->email = $request->email;
         $guest->message = $request->message;
         $guest->save();
-        $data = ['guestName' => $request->name];
-        Mail::send('contact_mail', $data, function($message) use ($request){
-            $message->to($request->email, $request->name);
-            $message->subject('Website Contact Form: ' . $request->name);
-            $message->from('huydq2510@gmail.com', 'Quang Huy');
-        });
+        return true;
     }
 }
